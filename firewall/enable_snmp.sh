98f406a99ccebacbae290d685af7959dc8a78033
Kojiro ONO <ono.kojiro@gmail.com>
2026-08-29 23:06:57 +0900

    enable/disable snmp

---
+++ firewall/enable_snmp.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. ./key_and_secret.shrc
+
+url=https://192.168.122.99/api/netsnmp/general/set
+
+cat - << EOF > data.json
+{
+  "general": {
+    "enabled": "1"
+  }
+}
+EOF
+
+curl -s -u "$key:$secret" -k -X POST \
+  -H "Content-Type: application/json" \
+  $url -d @data.json | jq .
+
+
+url=https://192.168.122.99/api/netsnmp/service/reconfigure
+
+curl -s -u "$key:$secret" -k -X POST $url | jq .
+
+
